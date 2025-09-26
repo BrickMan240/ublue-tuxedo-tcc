028ec130018f083214e2dae9a112351aca7d030e
Okazakee <okazakee@proton.me>
2025-09-26 18:24:16 +0200

    added signing

--- build.sh
+++ build.sh
@@ -28,8 +28,9 @@ rpm-ostree install tuxedo-drivers
 # Prebuild kernel modules for this system
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
-# Build the modules manually
-cd /usr/src/tuxedo-drivers-4.15.4
+# Copy source to writable location and build modules
+cp -r /usr/src/tuxedo-drivers-4.15.4 /tmp/tuxedo-drivers-build
+cd /tmp/tuxedo-drivers-build
 
 # Build all available modules
 make -C /lib/modules/${KERNEL_VERSION}/build M=$(pwd) modules
@@ -37,7 +38,14 @@ make -C /lib/modules/${KERNEL_VERSION}/build M=$(pwd) modules
 # Install the built modules
 make -C /lib/modules/${KERNEL_VERSION}/build M=$(pwd) modules_install
 
+# Sign the modules for Secure Boot compatibility
+for module in $(find /lib/modules/${KERNEL_VERSION}/updates -name "*.ko" 2>/dev/null); do
+    /usr/src/kernels/${KERNEL_VERSION}/scripts/sign-file sha256 /usr/src/kernels/${KERNEL_VERSION}/certs/signing_key.pem /usr/src/kernels/${KERNEL_VERSION}/certs/signing_key.x509 "$module" 2>/dev/null || true
+done
+
+# Clean up
 cd /
+rm -rf /tmp/tuxedo-drivers-build
 
 #Hacky workaround to make TCC install elsewhere
 mkdir -p /usr/share
